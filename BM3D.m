35723fe10d9225122831a0a66166cc9715c6701d
tturkerunlu <turkalpunlu@hotmail.com>
2026-04-14 19:31:48 +0300

    Add files via upload

---
+++ BM3D.m
@@ -0,0 +1,278 @@
+function [PSNR, y_est] = BM3D(y, z, sigma, profile, print_to_screen)
+image_name = [
+     'input.png'
+    ];
+
+if (exist('profile') ~= 1)
+    profile         = 'np';
+end
+
+if (exist('sigma') ~= 1),
+    sigma               = 25;
+end
+
+transform_2D_HT_name     = 'bior1.5'; 
+transform_2D_Wiener_name = 'dct';     
+transform_3rd_dim_name   = 'haar'; 
+
+N1                  = 8;
+Nstep               = 2;
+N2                  = 16;
+Ns                  = 39;  
+tau_match           = 3000;
+lambda_thr2D        = 0;
+lambda_thr3D        = 2.6;
+beta                = 2.0;
+
+N1_wiener           = 8;
+Nstep_wiener        = 2;
+N2_wiener           = 32;
+Ns_wiener           = 39;
+tau_match_wiener    = 400;
+beta_wiener         = 2.0;
+
+stepFS              = 1;
+smallLN             = 'not used in np';
+stepFSW             = 1;
+smallLNW            = 'not used in np';
+thrToIncStep        = 8;             
+
+if strcmp(profile, 'lc') == 1,
+
+    Nstep               = 6;
+    Ns                  = 25;
+    Nstep_wiener        = 5;
+    N2_wiener           = 16;
+    Ns_wiener           = 25;
+
+    thrToIncStep        = 3;
+    smallLN             = 3;
+    stepFS              = 6*Nstep;
+    smallLNW            = 2;
+    stepFSW             = 5*Nstep_wiener;
+
+end
+
+if (strcmp(profile, 'vn') == 1) | (sigma > 40),
+
+    N2                  = 32;
+    Nstep               = 4;
+ 
+    N1_wiener           = 11;
+    Nstep_wiener        = 6;
+
+    lambda_thr3D        = 2.8;
+    thrToIncStep        = 3;
+    tau_match_wiener    = 3500;
+    tau_match           = 25000;
+    
+    Ns_wiener           = 39;
+    
+end
+
+if (strcmp(profile, 'vn_old') == 1) & (sigma > 40),
+
+    transform_2D_HT_name = 'dct'; 
+    
+    N1                  = 12;
+    Nstep               = 4;
+ 
+    N1_wiener           = 11;
+    Nstep_wiener        = 6;
+
+    lambda_thr3D        = 2.8;
+    lambda_thr2D        = 2.0;
+    thrToIncStep        = 3;
+    tau_match_wiener    = 3500;
+    tau_match           = 5000;
+    
+    Ns_wiener           = 39;
+    
+end
+
+decLevel = 0;        
+thr_mask = ones(N1); 
+
+if strcmp(profile, 'high') == 1,
+    
+    decLevel     = 1; 
+    Nstep        = 2;
+    Nstep_wiener = 2;
+    lambda_thr3D = 2.5;
+    vMask = ones(N1,1); vMask((end/4+1):end/2)= 1.01; vMask((end/2+1):end) = 1.07;
+    thr_mask = vMask * vMask'; 
+    beta         = 2.5;
+    beta_wiener  = 1.5;
+    
+end
+
+dump_output_information = 1;
+if (exist('print_to_screen') == 1) & (print_to_screen == 0),
+    dump_output_information = 0;
+end
+
+[Tfor, Tinv]   = getTransfMatrix(N1, transform_2D_HT_name, decLevel);
+[TforW, TinvW] = getTransfMatrix(N1_wiener, transform_2D_Wiener_name, 0);
+
+if (strcmp(transform_3rd_dim_name, 'haar') == 1) | (strcmp(transform_3rd_dim_name(end-2:end), '1.1') == 1),
+    hadper_trans_single_den         = {};
+    inverse_hadper_trans_single_den = {};
+else
+    for hpow = 0:ceil(log2(max(N2,N2_wiener))),
+        h = 2^hpow;
+        [Tfor3rd, Tinv3rd]   = getTransfMatrix(h, transform_3rd_dim_name, 0);
+        hadper_trans_single_den{h}         = single(Tfor3rd);
+        inverse_hadper_trans_single_den{h} = single(Tinv3rd');
+    end
+end
+
+if beta_wiener==2 & beta==2 & N1_wiener==8 & N1==8
+    Wwin2D = [ 0.1924    0.2989    0.3846    0.4325    0.4325    0.3846    0.2989    0.1924;
+        0.2989    0.4642    0.5974    0.6717    0.6717    0.5974    0.4642    0.2989;
+        0.3846    0.5974    0.7688    0.8644    0.8644    0.7688    0.5974    0.3846;
+        0.4325    0.6717    0.8644    0.9718    0.9718    0.8644    0.6717    0.4325;
+        0.4325    0.6717    0.8644    0.9718    0.9718    0.8644    0.6717    0.4325;
+        0.3846    0.5974    0.7688    0.8644    0.8644    0.7688    0.5974    0.3846;
+        0.2989    0.4642    0.5974    0.6717    0.6717    0.5974    0.4642    0.2989;
+        0.1924    0.2989    0.3846    0.4325    0.4325    0.3846    0.2989    0.1924];
+    Wwin2D_wiener = Wwin2D;
+else
+    Wwin2D           = kaiser(N1, beta) * kaiser(N1, beta)';
+    Wwin2D_wiener    = kaiser(N1_wiener, beta_wiener) * kaiser(N1_wiener, beta_wiener)';
+end
+if (exist('y') ~= 1) | (exist('z') ~= 1)
+    y        = im2double(imread(image_name));
+    randn('seed', 0); 
+    z        = y + (sigma/255)*randn(size(y));
+else
+    
+    image_name = 'External image';
+    
+    z = double(z);
+    
+    y = double(y);
+    
+    if (max(z(:)) > 10),
+        z = z / 255;
+    end
+    
+    if (max(y(:)) > 10),
+        y = y / 255;
+    end
+end
+
+
+
+if (size(z,3) ~= 1) | (size(y,3) ~= 1),
+    error('BM3D accepts only grayscale 2D images.');
+end
+
+
+y_is_invalid_image = (length(size(z)) ~= length(size(y))) | (size(z,1) ~= size(y,1)) | (size(z,2) ~= size(y,2));
+if (y_is_invalid_image),
+    dump_output_information = 0;
+end
+
+if dump_output_information == 1,
+    fprintf('Image: %s (%dx%d), sigma: %.1f\n', image_name, size(z,1), size(z,2), sigma);
+end
+
+tic;
+y_hat = bm3d_thr(z, hadper_trans_single_den, Nstep, N1, N2, lambda_thr2D,...
+	lambda_thr3D, tau_match*N1*N1/(255*255), (Ns-1)/2, (sigma/255), thrToIncStep, single(Tfor), single(Tinv)', inverse_hadper_trans_single_den, single(thr_mask), Wwin2D, smallLN, stepFS );
+estimate_elapsed_time = toc;
+
+if dump_output_information == 1,
+    PSNR_INITIAL_ESTIMATE = 10*log10(1/mean((y(:)-double(y_hat(:))).^2));
+    fprintf('BASIC ESTIMATE, PSNR: %.2f dB\n', PSNR_INITIAL_ESTIMATE);
+end
+
+tic;
+y_est = bm3d_wiener(z, y_hat, hadper_trans_single_den, Nstep_wiener, N1_wiener, N2_wiener, ...
+    'unused arg', tau_match_wiener*N1_wiener*N1_wiener/(255*255), (Ns_wiener-1)/2, (sigma/255), 'unused arg', single(TforW), single(TinvW)', inverse_hadper_trans_single_den, Wwin2D_wiener, smallLNW, stepFSW, single(ones(N1_wiener)) );
+wiener_elapsed_time = toc;
+
+y_est = double(y_est);
+
+PSNR = 0;
+if (~y_is_invalid_image),
+    PSNR = 10*log10(1/mean((y(:)-y_est(:)).^2));
+end
+
+if dump_output_information == 1,
+    fprintf('FINAL ESTIMATE (total time: %.1f sec), PSNR: %.2f dB\n', ...
+        wiener_elapsed_time + estimate_elapsed_time, PSNR);
+
+    figure, imshow(z); title(sprintf('Noisy %s, PSNR: %.3f dB (sigma: %d)', ...
+        image_name(1:end-4), 10*log10(1/mean((y(:)-z(:)).^2)), sigma));
+
+    figure, imshow(y_est); title(sprintf('Denoised %s, PSNR: %.3f dB', ...
+        image_name(1:end-4), PSNR));
+    
+end
+
+return;
+
+function [Tforward, Tinverse] = getTransfMatrix (N, transform_type, dec_levels)
+
+if exist('dec_levels') ~= 1,
+    dec_levels = 0;
+end
+
+if N == 1,
+    Tforward = 1;
+elseif strcmp(transform_type, 'hadamard') == 1,
+    Tforward    = hadamard(N);
+elseif (N == 8) & strcmp(transform_type, 'bior1.5')==1
+    Tforward =  [ 0.353553390593274   0.353553390593274   0.353553390593274   0.353553390593274   0.353553390593274   0.353553390593274   0.353553390593274   0.353553390593274;
+       0.219417649252501   0.449283757993216   0.449283757993216   0.219417649252501  -0.219417649252501  -0.449283757993216  -0.449283757993216  -0.219417649252501;
+       0.569359398342846   0.402347308162278  -0.402347308162278  -0.569359398342846  -0.083506045090284   0.083506045090284  -0.083506045090284   0.083506045090284;
+      -0.083506045090284   0.083506045090284  -0.083506045090284   0.083506045090284   0.569359398342846   0.402347308162278  -0.402347308162278  -0.569359398342846;
+       0.707106781186547  -0.707106781186547                   0                   0                   0                   0                   0                   0;
+                       0                   0   0.707106781186547  -0.707106781186547                   0                   0                   0                   0;
+                       0                   0                   0                   0   0.707106781186547  -0.707106781186547                   0                   0;
+                       0                   0                   0                   0                   0                   0   0.707106781186547  -0.707106781186547];   
+elseif (N == 8) & strcmp(transform_type, 'dct')==1
+    Tforward = [ 0.353553390593274   0.353553390593274   0.353553390593274   0.353553390593274   0.353553390593274   0.353553390593274   0.353553390593274   0.353553390593274;
+       0.490392640201615   0.415734806151273   0.277785116509801   0.097545161008064  -0.097545161008064  -0.277785116509801  -0.415734806151273  -0.490392640201615;
+       0.461939766255643   0.191341716182545  -0.191341716182545  -0.461939766255643  -0.461939766255643  -0.191341716182545   0.191341716182545   0.461939766255643;
+       0.415734806151273  -0.097545161008064  -0.490392640201615  -0.277785116509801   0.277785116509801   0.490392640201615   0.097545161008064  -0.415734806151273;
+       0.353553390593274  -0.353553390593274  -0.353553390593274   0.353553390593274   0.353553390593274  -0.353553390593274  -0.353553390593274   0.353553390593274;
+       0.277785116509801  -0.490392640201615   0.097545161008064   0.415734806151273  -0.415734806151273  -0.097545161008064   0.490392640201615  -0.277785116509801;
+       0.191341716182545  -0.461939766255643   0.461939766255643  -0.191341716182545  -0.191341716182545   0.461939766255643  -0.461939766255643   0.191341716182545;
+       0.097545161008064  -0.277785116509801   0.415734806151273  -0.490392640201615   0.490392640201615  -0.415734806151273   0.277785116509801  -0.097545161008064];
+elseif (N == 8) & strcmp(transform_type, 'dst')==1
+    Tforward = [ 0.161229841765317   0.303012985114696   0.408248290463863   0.464242826880013   0.464242826880013   0.408248290463863   0.303012985114696   0.161229841765317;
+       0.303012985114696   0.464242826880013   0.408248290463863   0.161229841765317  -0.161229841765317  -0.408248290463863  -0.464242826880013  -0.303012985114696;
+       0.408248290463863   0.408248290463863                   0  -0.408248290463863  -0.408248290463863                   0   0.408248290463863   0.408248290463863;
+       0.464242826880013   0.161229841765317  -0.408248290463863  -0.303012985114696   0.303012985114696   0.408248290463863  -0.161229841765317  -0.464242826880013;
+       0.464242826880013  -0.161229841765317  -0.408248290463863   0.303012985114696   0.303012985114696  -0.408248290463863  -0.161229841765317   0.464242826880013;
+       0.408248290463863  -0.408248290463863                   0   0.408248290463863  -0.408248290463863                   0   0.408248290463863  -0.408248290463863;
+       0.303012985114696  -0.464242826880013   0.408248290463863  -0.161229841765317  -0.161229841765317   0.408248290463863  -0.464242826880013   0.303012985114696;
+       0.161229841765317  -0.303012985114696   0.408248290463863  -0.464242826880013   0.464242826880013  -0.408248290463863   0.303012985114696  -0.161229841765317];
+elseif strcmp(transform_type, 'dct') == 1,
+    Tforward    = dct(eye(N));
+elseif strcmp(transform_type, 'dst') == 1,
+    Tforward    = dst(eye(N));
+elseif strcmp(transform_type, 'DCrand') == 1,
+    x = randn(N); x(1:end,1) = 1; [Q,R] = qr(x); 
+    if (Q(1) < 0), 
+        Q = -Q; 
+    end;
+    Tforward = Q';
+else 
+    
+    dwtmode('per','nodisp');  
+    
+    Tforward = zeros(N,N);
+    for i = 1:N
+        Tforward(:,i)=wavedec(circshift([1 zeros(1,N-1)],[dec_levels i-1]), log2(N), transform_type);
+    end
+end
+
+Tforward = (Tforward' * diag(sqrt(1./sum(Tforward.^2,2))))'; 
+
+Tinverse = inv(Tforward);
+
+return;
+
